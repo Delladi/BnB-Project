7ae45138ca7c629462d56636375ffca8179b5acc
jannisjr <joe_hannis@hotmail.com>
2023-06-06 17:18:14 +0100

    fixed routing in app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'active_record'
-#require 'database_connection'
+require_relative 'lib/database_connection'
+require_relative 'lib/user'
 
-# DatabaseConection.establish_database_connection
+DatabaseConnection.establish_database_connection
 
 class Application < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
@@ -18,8 +20,41 @@ class Application < Sinatra::Base
     return erb(:signup)
   end
 
+  post '/signup' do
+    database = User.all_records
+    match_found = false
+  
+    database.each do |record|
+      if record.username == params[:username] || record.email == params[:email]
+        match_found = true
+        break
+      end
+    end
+  
+    if match_found
+      redirect '/signup'
+    else
+      User.create_user(params[:name], params[:username], params[:email], params[:password])
+      redirect '/success'
+    end
+  end
+
   get '/login' do
     return erb(:login)
+  end
+
+  get '/account_page' do
+    return erb(:account_page)
+  end
+
+  post '/login' do
+    user = User.sign_in(params[:username], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/account_page'
+    else
+      redirect '/login_failure'
+    end
   end
 
   get '/bookings' do
@@ -32,5 +67,9 @@ class Application < Sinatra::Base
 
   get '/1' do
     return erb(:stylish_cottage_getaway)
+  end
+
+  get '/success' do
+    return erb(:success)
   end
 end
